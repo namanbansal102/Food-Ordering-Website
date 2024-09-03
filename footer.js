251f200cc6fbc12b7fae5443a1ec40b6f81ab5a1
NITISH <nitishmehan0608@gmail.com>
2024-09-03 15:32:35 +0530

    Jai mata di

--- footer.js
+++ footer.js
@@ -74,3 +74,22 @@ document.querySelector('.s-4').addEventListener('click', () => {
     `;
     s4=1;
 });
+let menu_state=0;
+document.querySelector('.more').addEventListener('click',()=>{
+    if(menu_state===0){
+
+        document.querySelector('#side-menu').style.left='0';
+        menu_state=1;
+        console.log("open");
+        return;
+    }
+})
+
+document.querySelector('#close-btn').addEventListener('click',()=>{
+    if(menu_state===1){
+        document.querySelector('#side-menu').style.left='-100%';
+        menu_state=0;
+        console.log("close");
+    }
+})
+

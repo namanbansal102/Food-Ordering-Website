5db8dcf5f83e748877b00db742cfce5bf6505fa8
Nakul <nakulg616@gmail.com>
2024-09-03 10:33:59 +0530

    ALL done

--- blog.js
+++ blog.js
@@ -1,44 +1,44 @@
 document.getElementById('openFileDiv-1').addEventListener('click', function() {
-    // Path to the local file
+    
     var filePath = 'reviews/review_1.html';
     
-    // Navigating to the local file
+    
     window.location.href = filePath;
 });
 
 document.getElementById('openFileDiv-2').addEventListener('click', function() {
-    // Path to the local file
+    
     var filePath = 'reviews/review_2.html';
     
-    // Navigating to the local file
+    
     window.location.href = filePath;
 });
 document.getElementById('openFileDiv-3').addEventListener('click', function() {
-    // Path to the local file
+   
     var filePath = 'reviews/review_3.html';
     
-    // Navigating to the local file
+   
     window.location.href = filePath;
 });
 document.getElementById('openFileDiv-4').addEventListener('click', function() {
-    // Path to the local file
+    
     var filePath = 'reviews/review_4.html';
     
-    // Navigating to the local file
+    
     window.location.href = filePath;
 });
 document.getElementById('openFileDiv-5').addEventListener('click', function() {
-    // Path to the local file
+ 
     var filePath = 'reviews/review_5.html';
     
-    // Navigating to the local file
+  
     window.location.href = filePath;
 });
 document.getElementById('openFileDiv-6').addEventListener('click', function() {
-    // Path to the local file
+   
     var filePath = 'reviews/review_6.html';
     
-    // Navigating to the local file
+   
     window.location.href = filePath;
 });
 
